c7f265b776fd7d773eae608e54688dd508a07696
Viviane Chan <vivianechan@vivianes-mbp.media.in.cbcsrc.ca>
2017-01-31 16:21:41 -0500

    Storyboard + ViewController + CustomCell. WIP

--- CBCSearch/ViewController.swift
+++ CBCSearch/ViewController.swift
@@ -8,17 +8,126 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate, UICollectionViewDelegateFlowLayout {
 
+    
+    // MARK: Properties
+    var lineUpModel: LineupModel!
+    var lineUpModelArray:[LineupModel] = []
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    var searchController: UISearchController = UISearchController(searchResultsController: nil)
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchController.searchBar.delegate = self
+        prepareLayout()
+        parseJSON()
+    }
+    
+    
+    //MARK: Prepare Layout
+    
+    func prepareLayout(){
+        searchBar.barTintColor = UIColor.red
+        let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
+        if statusBar.responds(to: #selector(setter: UIView.backgroundColor)) {
+            statusBar.backgroundColor = UIColor.red
+        }
+        for view in searchBar.subviews {
+            for subview in view.subviews {
+                if subview.isKind(of: UITextField.self) {
+                    let textField: UITextField = subview as! UITextField
+                    textField.backgroundColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+                        textField.textColor = UIColor.white
+                    }
+                }
+            }
+        }
+  
+
+    
+
+    //MARK: Parse Json
+    
+    func parseJSON(){
+        let requestURL: URL = URL(string: "https://api-gw.radio-canada.ca/aggregate-content/v1/items?q=trump&sortOrder=byRecency")!
+        let urlRequest: NSMutableURLRequest = NSMutableURLRequest(url: requestURL)
+        let session = URLSession.shared
+        let task = session.dataTask(with: urlRequest as URLRequest) {
+            (data, response, error) -> Void in
+            let httpResponse = response as! HTTPURLResponse
+            let statusCode = httpResponse.statusCode
+            if (statusCode == 200) {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                    
+                    if let articles = json as? [[String: AnyObject]] {
+                        for article in articles {
+                            if let title = article["title"] as? String {
+                                if let typeAttribute = article["typeAttributes"] as? [String:AnyObject] {
+                                    if let picture = typeAttribute["imageSmall"] as? String {
+                                        if let publishedDate = article["publishedAt"] as? Int {
+                                            var lineupItem = LineupItemModel(title: title)
+                                            let date = NSDate(timeIntervalSince1970: TimeInterval(publishedDate))
+                                            let urlstring = picture
+                                            let url = NSURL(string: urlstring)
+                                            lineupItem.imageURL = url as URL?
+                                            self.lineUpModelArray.append(lineupItem)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                        DispatchQueue.main.async(execute: {
+                        self.collectionView.reloadData()
+                    })
+                }
+                catch {
+                    print("error serializing JSON: \(error)")
+                }
+            }
+            print(self.lineUpModelArray)
+        }
+        task.resume()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: CollectionView 
+    
+    // Cell size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (self.view.frame.width - 30), height:100)
     }
+    
+    // Datasource
+
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.lineUpModelArray.count
+    }
+
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "searchCell", for: indexPath) as! SearchCollectionViewCell
+        
+        let unformattedDate = self.lineUpModelArray[indexPath.row].date
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        let formattedDate = formatter.string(from: unformattedDate as Date)
+        cell.timeLabel.text = String(describing: formattedDate)
+        
+        cell.imageView.image = UIImage(data: try! Data(contentsOf: self.lineUpModelArray[indexPath.row].imageURL!))
+        cell.titleLabel.text = self.lineUpModelArray[indexPath.row].title
+        
+        return cell
+    }
+    
+
 
 
 }
