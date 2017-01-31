34e7df39e611ec7c22e20ebb2a2fa0742e81e5bc
Jeremy Evans <jeremy.evans@cbc.ca>
2017-01-31 12:03:19 -0500

    Upgrade project for cocoapod support

---
+++ CBCSearch/LineupDataDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  LineupDataDelegate.swift
+//  CBCSearch
+//
+//  Created by Jeremy Evans on 2017-01-31.
+//  Copyright © 2017 cbc. All rights reserved.
+//
+
+import Foundation
--- CBCSearch/VideoLineupItemModel.swift
+++ CBCSearch/VideoLineupItemModel.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import LineupItemModel
 
 protocol VideoModel : LineupModel {
     var videoURL :URL { get set}
